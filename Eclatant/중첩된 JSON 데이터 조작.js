e5dabb989290bd7e397ff0f184a2c1ac71a4896c
Jeewhan <distinct19@gmail.com>
2017-10-18 14:53:35 +0900

    Create checkExpectation()
    * 단순하게 생각하면 childnode가 빈 배열인지 여부를 검사하여 재귀함수의 종단점을 설정해줄 수 있게 해주는 함수입니다.
    * if 문 안에서 바로 length를 검사할 때에 비해 차후 요구사항 변경시에 대응하기 수월할 것으로 예상됩니다.

--- Eclatant/중첩된 JSON 데이터 조작.js
+++ Eclatant/중첩된 JSON 데이터 조작.js
@@ -9,3 +9,16 @@ function checkExistenceForAdd(store, prop, data) {
 
   store[prop].push(data);
 }
+
+// argument인 target이 array가 아닐 수 있다면 주석해제
+function checkExpectation(target) {
+  if (
+    // typeof target === "object" &&
+    // target.constructor === Array &&
+    target.length > 0
+  ) {
+    return true;
+  }
+
+  return false;
+}
