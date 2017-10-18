5f1fcc32a6f87997e0cce3003d88de1ffa3c1a35
crispynap <crispynap@gmail.com>
2017-10-18 13:36:13 +0900

    리드라인 도입, 코드 설계

---
+++ vending-machine-app/config/readline.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+	var readline = require('readline');
+
+	var r = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	r.setPrompt('> ');
+	r.prompt();
+
+	r.on('close', function () {
+		process.exit();
+	});
+	return r;
+}
