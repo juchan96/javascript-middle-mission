b6ad09ad9c13488364c89619b77f3553d081ed79
Jeewhan <distinct19@gmail.com>
2017-10-19 14:47:27 +0900

    Refactoring recursive()
    * rename recursive -> search
    * childnode가 빈 배열일 경우 return

--- Eclatant/중첩된 JSON 데이터 조작.js
+++ Eclatant/중첩된 JSON 데이터 조작.js
@@ -10,35 +10,22 @@ function checkExistenceForAdd(store, prop, data) {
   store[prop].push(data);
 }
 
-// argument인 target이 array가 아닐 수 있다면 주석해제
-function checkExpectation(target) {
-  if (
-    // typeof target === "object" &&
-    // target.constructor === Array &&
-    target.length > 0
-  ) {
-    return true;
-  }
-
-  return false;
-}
-
-function recursive(store, data, typeValue) {
+function search(store, data, typeValue) {
   if (data.type === typeValue) {
     checkExistenceForAdd(store, typeValue, data.name);
   }
 
-  if (checkExpectation(data.childnode)) {
-    data.childnode.forEach(function(child) {
-      recursive(store, child, typeValue);
-    });
-  } else {
-    return false;
+  if (data.childnode.length === 0) {
+    return;
   }
+
+  data.childnode.forEach(function(child) {
+    search(store, child, typeValue);
+  });
 }
 
 function parse(jsonData, typeValue) {
-  recursive(resultObj, jsonData[0], typeValue);
+  search(resultObj, jsonData[0], typeValue);
 }
 
 parse(jsonData, "sk");
