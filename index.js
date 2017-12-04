bb7970511e8a179856feb54b9266167d0075c75a
hannarita <harugio@naver.com>
2017-12-04 12:04:19 +0900

    add: drinks object

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
+var getReadLine = (function () {
+    var readline = require('readline');
+
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return function () {
+        return rl;
+    }
+})();
+
 var vendingMachine = {
-    drinks : [],
+    drinks : [
+        {
+            name: '데자와',
+            price: 1200,
+            stock: 3
+        },
+        {
+            name: '쿠우',
+            price: 1000,
+            stock: 3
+        },
+        {
+            name: '오이오녹차',
+            price: 2000,
+            stock: 3
+        },
+        {
+            name: '오후의홍차',
+            price: 1300,
+            stock: 3
+        },
+        {
+            name: '제티',
+            price: 600,
+            stock: 3
+        }
+    ],
     inputMoney : function () {
         
     },
@@ -9,10 +48,10 @@ var vendingMachine = {
     selectDrink : function () {
         
     },
-    returnMoney : function () {
-        
-    },
     disposeMoney : function () {
+
+    },
+    returnMoney : function () {
         
     }
 };
