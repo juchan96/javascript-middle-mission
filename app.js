7891dd13ae1bcea14de06aa7b7444bb25a436e75
Dongwoo Lee <likedemian@me.com>
2018-01-12 16:59:05 +0900

    Fix: 자판기 함수 내 중첩문 분리 & READLINE.question 재호출

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const READLINE = require('readline').createInterface({
   output: process.stdout
 });
 
-let MONEY = 0;
 const DATA = [{
   'name': '콜라',
   'price': 1000,
@@ -35,79 +34,65 @@ const DATA = [{
 }]
 
 
-const insertCoin = (obj) => {
+const insertCoin = () => {
   READLINE.question('동전을 넣으세요: ', (coin) => {
-    coin = Number(coin);
-    if (typeof (coin) === 'number') {
-      obj += coin;
-      console.log('투입금액:', obj + "원");
-      availableBeverages(obj);
+    coin *= 1;
+    if (typeof (coin) === 'number' && !isNaN(coin)) {
+      console.log('투입금액:', coin + "원");
+      availableBeverages(coin);
     } else {
-      console.log('금액은 숫자만 입력해 주세요');
-      insertCoin(obj)
+      console.log('숫자만 입력하세요');
+      insertCoin()
     }
   })
 }
-
 
 
 const availableBeverages = (obj) => {
   beverages = [];
   for (let key in DATA) {
     let availables = DATA[key];
-    if (availables.stock > 0) {
-      beverages.push(availables)
-    }
+    beverages.push(availables)
   }
   console.log("선택 가능 음료:", beverages);
   selectBeverages(obj, beverages)
 }
 
 
-
-
 const selectBeverages = (obj, arr) => {
-  READLINE.question('음료를 선택하세요, 반환하시려면 <반환>이라고 입력해주세요: ', (name) => {
+  READLINE.question('음료를 선택하세요: ', (name) => {
     for (let elem in arr) {
-      if (arr[elem].name === name) {
-        console.log(name + "을 선택했습니다.");
-        if (obj >= arr[elem].price || obj !== 0) {
-          arr[elem].stock--;
-          let change = obj - arr[elem].price
-          if (arr[elem].stock < 1) {
-            READLINE.question('음료를 구입했습니다. 다른 음료를 구입하시겠습니까?: (Y/N)', (answer) => {
-              if (answer === 'Y') {
-                availableBeverages(change)
-                selectBeverages(change, arr)
-              } else {
-                console.log("이용해주셔서 감사합니다.");
-                READLINE.close()
-              }
-            })
-          } else {
-            console.log("거스름돈:", change + "원");
-            READLINE.question('음료를 구입했습니다. 다른 음료를 구입하시겠습니까?: (Y/N) ', (answer) => {
-              if (answer === 'Y' || answer ===  'y') {
-                availableBeverages(change)
-                selectBeverages(change, arr)
-              } else {
-                console.log("이용해주셔서 감사합니다.");
-                READLINE.close()
-              }
-            })
-          }
-        } else {
-          console.log("투입 금액이 부족합니다");
-          insertCoin(obj);
-        }
+      let bev = arr[elem]
+      let change = obj - bev.price;
+      if (name === bev.name) {
+        statusOfBeverage(name, bev, obj, change, arr)
       }
-    }
-    if (name === '반환') {
-      console.log("반환금액은" + obj + "원입니다. 이용해주셔서 감사합니다.");
-
-      READLINE.close()
     }
   })
 }
 
-insertCoin(MONEY);
+
+const statusOfBeverage = (name, bev, obj, change, arr) => {
+  if (bev.stock === 0) {
+    console.log("재고가 없습니다.");
+    selectBeverages(obj, arr)
+  } else if (obj < bev.price || change < 0) {
+    console.log("투입 금액이 부족합니다");
+    insertCoin(obj);
+  } else {
+    console.log(name + "을 구입했습니다. 잔액은 " + change + "원 입니다.");
+    --bev.stock;
+    purchaseAnother(change)
+  }
+}
+
+
+const purchaseAnother = (obj, arr) => {
+  READLINE.question('다른 음료를 구매할까요 반환할까요?: (구매 / 반환) ', (answer) => {
+    (answer === '구매') ? availableBeverages(obj) && selectBeverages(obj, arr):
+      (answer === '반환') ? READLINE.close() : purchaseAnother(obj, arr)
+  })
+}
+
+
+insertCoin();
