3557a627213b6afa9ca0622908b3ee8fb393d63c
Dongwoo Lee <likedemian@me.com>
2018-01-17 11:05:10 +0900

    Add: an operating lotto function

---
+++ lotto.js
@@ -0,0 +1,112 @@
+// pick을 모아서 하나의 픽스에 담아둔다.
+// 픽스:[[],[],[],,,]
+// 픽스와 당첨번호를 인자로 받는 함수를 만들어서,
+// 그 함수에서는 이중배열을 처리할 수 있게 반복문을 구현한다.
+// 반복을 하면서 당첨번호와 일치하는 번호가 있는지 확인한다
+// 일치하면 일치하는 갯수를 추가한다.
+let PRIZEMONEY = 0;
+let PICKS = [];
+let RESULT = [];
+
+
+const READLINE = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const purchaseTickets = (money) => {
+  READLINE.question('구입금액을 1000원 단위로 입력해 주세요: ', (money) => {
+    let tickets = money / 1000
+    console.log(tickets + "개를 구매했습니다.");
+    for (var i = 0; i < tickets; i++) {
+      PICKS.push(insertNum(i));
+    }
+    console.log(PICKS);
+    checkResult(money);
+  });
+}
+
+
+const insertNum = () => {
+  let lotto = [];
+  let temp;
+  let random;
+  for (let i = 1; i <= 45; i++) {
+    lotto.push(i);
+  }
+  for (let i = 0; i < lotto.length; i++) {
+    random = Math.floor(Math.random() * 45);
+    temp = lotto[i];
+    lotto[i] = lotto[random];
+    lotto[random] = temp;
+  }
+  let pick = lotto.slice(0, 6).sort(ascSorting);
+  return pick;
+}
+
+
+const checkResult = (money) => {
+  READLINE.question('지난 주 당첨번호를 입력해 주세요: ', (lottoNum) => {
+    let lottoArray = lottoNum.split(',') || lottoNum.split(', ');
+    for (var i = 0; i < lottoArray.length; i++) {
+      RESULT.push(Number(lottoArray[i]));
+    }
+    compareResult(money)
+    READLINE.close();
+  });
+}
+
+
+const compareResult = (money) => {
+  let arr = [];
+  let result = {};
+  let threeCount = 0;
+  let fourCount = 0;
+  let fiveCount = 0;
+  let sixCount = 0;
+
+  for (let i = 0; i < RESULT.length; i++) {
+    for (let j = 0; j < PICKS.length; j++) {
+      if (PICKS[j].indexOf(RESULT[i]) !== -1) {
+        arr.push(j);
+        arr.sort(ascSorting)
+      }
+    }
+  }
+  for (let val in arr) {
+    let idx = arr[val];
+    result[idx] = result[idx] === undefined ? 1 : result[idx] += 1;
+  }
+  for (let val in result) {
+    if (result[val] === 3) {
+      threeCount++;
+      PRIZEMONEY += 5000;
+    } else if (result[val] === 4) {
+      fourCount++;
+      PRIZEMONEY += 50000;
+    } else if (result[val] === 5) {
+      fiveCount++;
+      PRIZEMONEY += 1500000;
+    } else if (result[val] === 6) {
+      sixCount++;
+      PRIZEMONEY += 2000000000;
+    }
+  }
+  console.log("당첨 통계");
+  console.log("---------")
+  console.log("3개 일치 (5000원) : " + threeCount + "개");
+  console.log("4개 일치 (50000원) : " + fourCount + "개");
+  console.log("5개 일치 (1500000원) : " + fiveCount + "개");
+  console.log("6개 일치 (2000000000원) : " + sixCount + "개");
+  console.log("나의 수익률은 ", (((PRIZEMONEY / money) * 100) - 100) + "% 입니다.");
+}
+
+
+
+const ascSorting = (a, b) => {
+  return a - b;
+}
+
+
+purchaseTickets();
