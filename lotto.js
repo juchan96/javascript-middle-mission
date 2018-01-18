a323bf206533d13297f4626031db09b9327b9fa0
Dongwoo Lee <likedemian@me.com>
2018-01-18 23:30:25 +0900

    Fix: 전역변수, key값 이름 수정 && 에러메시지 이름 수정 *& for문 -> forEach, map으로 변경 && 조건문을 객체형태로 변경

--- lotto.js
+++ lotto.js
@@ -5,15 +5,15 @@
 // 반복을 하면서 당첨번호와 일치하는 번호가 있는지 확인한다
 // 일치하면 일치하는 갯수를 추가한다.
 
-let INIT_OBJECTS = {
-  PRIZEMONEY: 0,
-  PICKS: [],
-  RESULT: [],
-  LOTTO: []
+let INIT = {
+  prize: 0,
+  picks: [],
+  result: [],
+  lotto: []
 }
 
 for (let i = 1; i <= 45; i++) {
-  INIT_OBJECTS.LOTTO.push(i);
+  INIT.lotto.push(i);
 }
 
 
@@ -34,12 +34,12 @@ const purchaseTickets = (money) => {
     if (reg.test(cash)) {
       console.log(tickets + "개를 구매했습니다.");
       for (var i = 0; i < tickets; i++) {
-        INIT_OBJECTS.PICKS.push(insertNum());
+        INIT.picks.push(insertNum());
       }
-      console.log(INIT_OBJECTS.PICKS);
+      console.log(INIT.picks);
       checkResult(cash);
     } else {
-      console.log("다시 입력해 주세요.");
+      console.log("금액은 1000원 단위로 입력해야 합니다. 다시 입력해 주세요.");
       purchaseTickets();
     }
   });
@@ -49,13 +49,13 @@ const purchaseTickets = (money) => {
 const insertNum = () => {
   let temp;
   let random;
-  for (let i = 0; i < INIT_OBJECTS.LOTTO.length; i++) {
+  for (let i = 0; i < INIT.lotto.length; i++) {
     random = Math.floor(Math.random() * 45);
-    temp = INIT_OBJECTS.LOTTO[i];
-    INIT_OBJECTS.LOTTO[i] = INIT_OBJECTS.LOTTO[random];
-    INIT_OBJECTS.LOTTO[random] = temp;
+    temp = INIT.lotto[i];
+    INIT.lotto[i] = INIT.lotto[random];
+    INIT.lotto[random] = temp;
   }
-  let pick = INIT_OBJECTS.LOTTO.slice(0, 6).sort(ascSorting);
+  let pick = INIT.lotto.slice(0, 6).sort(ascSorting);
   return pick;
 }
 
@@ -64,8 +64,8 @@ const checkResult = (money) => {
   READLINE.question('지난 주 당첨번호를 입력해 주세요: ', (lottoNum) => {
     let lottoArray = lottoNum.split(',') || lottoNum.split(', ');
     lottoArray.forEach(elem => {
-      INIT_OBJECTS.RESULT.push(Number(elem));
-    });    
+      INIT.result.push(Number(elem));
+    });
     compareResult(money)
     READLINE.close();
   });
@@ -74,52 +74,53 @@ const checkResult = (money) => {
 
 const compareResult = (money) => {
   let arr = [];
+  let prizes = [];
   let result = {};
-  let threeCount = 0;
-  let fourCount = 0;
-  let fiveCount = 0;
-  let sixCount = 0;
-  for (let i = 0; i < INIT_OBJECTS.RESULT.length; i++) {
-    for (let j = 0; j < INIT_OBJECTS.PICKS.length; j++) {
-      if (INIT_OBJECTS.PICKS[j].indexOf(INIT_OBJECTS.RESULT[i]) !== -1) {
-        arr.push(j);
+  let prizeResult = {};
+  let totalPrize = 0;
+
+  let getPrize = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: INIT.prize = 5000,
+    4: INIT.prize = 50000,
+    5: INIT.prize = 1500000,
+    6: INIT.prize = 200000000
+  }
+
+  INIT.result.map(element => {
+    INIT.picks.forEach(elem => {
+      if (elem.indexOf(element) !== -1) {
+        arr.push(elem);
         arr.sort(ascSorting)
       }
-    }
-  }
+    });
+  });
 
-  for (let i = 0; i < arr.length; i++) {
-    let idx = arr[i];
+
+  arr.forEach(elem => {
+    let idx = elem;
     result[idx] = result[idx] === undefined ? 1 : result[idx] += 1;
-  }
-
+  });
 
   for (let val in result) {
-    if (result[val] === 3) {
-      threeCount++;
-      INIT_OBJECTS.PRIZEMONEY += 5000;
-    } else if (result[val] === 4) {
-      fourCount++;
-      INIT_OBJECTS.PRIZEMONEY += 50000;
-    } else if (result[val] === 5) {
-      fiveCount++;
-      INIT_OBJECTS.PRIZEMONEY += 1500000;
-    } else if (result[val] === 6) {
-      sixCount++;
-      INIT_OBJECTS.PRIZEMONEY += 2000000000;
-    }
+    prizes.push(result[val]);
   }
 
+  prizes.forEach(elem => {
+    totalPrize += getPrize[elem];
+    let idx = elem;
+    prizeResult[idx] = prizeResult[idx] === undefined ? 1 : prizeResult[idx] += 1;
+  });
 
-  console.log("당첨 통계");
-  console.log("---------")
-  console.log("3개 일치 (5000원) : " + threeCount + "개");
-  console.log("4개 일치 (50000원) : " + fourCount + "개");
-  console.log("5개 일치 (1500000원) : " + fiveCount + "개");
-  console.log("6개 일치 (2000000000원) : " + sixCount + "개");
-  console.log("나의 수익률은 ", (((INIT_OBJECTS.PRIZEMONEY / money) * 100) - 100).toFixed(0) + "% 입니다.");
+
+  for (var key in prizeResult) {
+    console.log(key + "개 일치: " + prizeResult[key] + "개");
+  }
+
+  console.log("나의 수익률은 ", (((totalPrize / money) * 100) - 100).toFixed(0) + "% 입니다.");
 }
-
 
 
 const ascSorting = (a, b) => {
