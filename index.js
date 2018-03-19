01cdfef5968f12c9d94f7ceb768ccd5da5681001
Log91 <>
2018-03-19 12:34:09 +0900

    Add message object, add (var result)  before return
    1. 메세지 객체를 만들어서 함수마다 입력되어 있던 메세지들을 모아놨습니다
    2. 모든 함수에서 리턴하기 전에 result변수를 만들어 담은 다음에 return result;했습니다

--- index.js
+++ index.js
@@ -34,6 +34,24 @@
   // 투입된 돈을 누적하는 변수
   let accumCoin = 0;
 
+  // 메세지 객체
+  let message = {
+    SOLDOUT: '사용가능한 음료수 : 없음',
+    SHORT_OF_MONEY: '금액이 모자랍니다',
+    returnNoStock: function (item) {
+      return `${item}는 재고가 없습니다`
+    },
+    returnChange: function (change) {
+      return `잔돈 ${change}이 반환됐습니다`
+    },
+    returnSelectedItem: function (item, accumCoin) {
+      return `${item}가 나왔습니다  현재잔돈 : ${accumCoin},  ${getBuyableItem(accumCoin)}`;
+    },
+    returnGetBuyableItem: function (itemArray) {
+      return `사용가능한 음료수 목록 => ${itemArray.join(', ')}`;
+    }
+  };
+
   // 돈을 넣으면 accumCoin에 돈이 누적되고 accumCoin으로 살 수 있는 음료 종류 반환하는 함수
   let insertCoin = function (coin) {
     accumCoin += coin;
@@ -43,32 +61,35 @@
 
   // 투입된 돈으로 살 수 있는 품목을 리턴하는 함수
   function getBuyableItem(coin) {
-    if (coin < 500) return '사용가능한 음료수 : 없음';
-    let result = [];
+    if (coin < 500) return message.SOLDOUT;
+    let itemArray = [];
     for (key in beverageData) {
       let item;
       beverageData[key].stock > 0 ? item = `${key}(${beverageData[key].price})` :
         item = `${key}(재고없음)`;
       if (coin >= beverageData[key].price) {
-        result.push(item);
+        itemArray.push(item);
       }
     }
-    return `사용가능한 음료수 목록 => ${result.join(', ')}`;
+    let result = message.returnGetBuyableItem(itemArray);
+    return result;
   }
 
   // 음료를 고르는 함수
   function selectedItem(item) {
-    if (beverageData[item].stock <= 0) return `${item}는 재고가 없습니다.`;
-    if (beverageData[item].price > accumCoin) return `금액이 모자랍니다.`;
+    if (beverageData[item].stock <= 0) return message.returnNoStock(item);
+    if (beverageData[item].price > accumCoin) return message.SHORT_OF_MONEY;
     accumCoin -= beverageData[item].price;
-    return `${item}가 나왔습니다  현재잔돈 : ${accumCoin},  ${getBuyableItem(accumCoin)}`;
+    let result = message.returnSelectedItem(item, accumCoin);
+    return result;
   }
 
   // 잔돈을 반환하는 함수
   function returnMoney() {
     let change = accumCoin;
     accumCoin = 0;
-    return `잔돈 ${change}이 반환됐습니다`
+    let result = message.returnChange(change);
+    return result;
   }
 
   console.log(insertCoin(2000));
