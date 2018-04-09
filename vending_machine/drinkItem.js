2576cfe81d76df3d32f5e1f2c729d7e3ebc1714e
feanar <feanar729@gmail.com>
2018-04-09 21:43:52 +0900

    add: 전체 물품 데이터 파일로 따로 분리

---
+++ vending_machine/drinkItem.js
@@ -0,0 +1,39 @@
+var drinkItem = [
+  {
+    "item": "콜라",
+    "price": 1000,
+    "stock": 35
+  },
+  {
+    "item": "사이다",
+    "price": 1000,
+    "stock": 132
+  },
+  {
+    "item": "포도주스",
+    "price": 700,
+    "stock": 5
+  },
+  {
+    "item": "딸기우유",
+    "price": 1000,
+    "stock": 25
+  },
+  {
+    "item": "미에로화이바",
+    "price": 900,
+    "stock": 44
+  },
+  {
+    "item": "물",
+    "price": 250,
+    "stock": 44
+  },
+  {
+    "item": "파워에이드",
+    "price": 1300,
+    "stock": "재고없음"
+  }
+];
+
+module.exports = drinkItem;
