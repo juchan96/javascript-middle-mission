a89e6275612671cb558e6b88fb019c0d262041c7
feanar <feanar729@gmail.com>
2018-04-12 14:19:25 +0900

    modify: 주석처리된 데이터 해제

--- vending_machine/drinkItem.js
+++ vending_machine/drinkItem.js
@@ -13,28 +13,28 @@ var drinkItem = [
     "item": "파워에이드",
     "price": 1300,
     "stock": "재고없음"
+  },
+  {
+    "item": "포도주스",
+    "price": 700,
+    "stock": 5
+  },
+  {
+    "item": "딸기우유",
+    "price": 1000,
+    "stock": 25
+  },
+  {
+    "item": "미에로화이바",
+    "price": 900,
+    "stock": 44
+  },
+  {
+    "item": "물",
+    "price": 250,
+    "stock": 44
   }
 ];
 
-  // {
-  //   "item": "포도주스",
-  //   "price": 700,
-  //   "stock": 5
-  // },
-  // {
-  //   "item": "딸기우유",
-  //   "price": 1000,
-  //   "stock": 25
-  // },
-  // {
-  //   "item": "미에로화이바",
-  //   "price": 900,
-  //   "stock": 44
-  // },
-  // {
-  //   "item": "물",
-  //   "price": 250,
-  //   "stock": 44
-  // },
 
 module.exports = drinkItem;
