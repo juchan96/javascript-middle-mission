19da4b97ecd1ec9288b280bfe47d72f174e66238
HYEON(win) <antaehyeon@live.co.kr>
2018-04-13 11:34:00 +0900

    refactor : 피드백 반영
    var 대신에 let, const 사용 (let, const, var의 차이 확인)
    lotto의 switch 문 대체

--- lotto.js
+++ lotto.js
@@ -4,13 +4,20 @@
   돈을 넣으면 살 수 있는 만큼 로또를 구매할 수 있다. (buyLottos 함수)
   각 로또 번호는 6개다.
 */
-var lottos = [];
-var winCountCheckArray;
-var prizeMoney = {three: 5000, four: 50000, five: 1500000, six: 2000000000};
+
+const lottos = [];
+let winCountCheckArray;
+const prizeMoney = {three: 5000, four: 50000, five: 1500000, six: 2000000000};
+
+// Magic Number
+const three = 0;
+const four = 1;
+const five = 2;
+const six = 3;
 
 function buyLottos(money) {
-  var lottoNum = money / 1000;
-  var lotto;
+  let lottoNum = money / 1000;
+  let lotto;
 
   // 몇개를 발행했습니다
   // 1000으로 나누면 됨
@@ -37,8 +44,8 @@ function buyLottos(money) {
 
 function createLottosNumbers() {
 
-  var randNum;
-  var lotto = [];
+  let randNum;
+  const lotto = [];
 
   // 인자만큼 반복문 수행
   while (lotto.length !== 6) {
@@ -62,19 +69,6 @@ function createLottosNumbers() {
   return lotto;
 }
 
-function setLuckyNumber(array) {
-  // 6개의 값을 각각 하나씩 찾아야함
-  // 생각이 잘 안남
-
-  // 일단 한개의 데이터(숫자1개~6개까지)를 넘겨줌
-
-  for (var i=0; i<array.length; i++) {
-    findWinNumber(array[i]);
-  }
-
-  printWinningStatistics(winCountCheckArray);
-}
-
 function increaseWinNumberCount(num) {
 
   // 인자로 받아온 숫자를
@@ -84,8 +78,8 @@ function increaseWinNumberCount(num) {
   // 카운트 해주는 것임
   // 나중에 출력할 때 
 
-  var lottoNum = lottos.length;
-  for (var i=0; i<lottoNum; i++) {
+  const lottoNum = lottos.length;
+  for (let i=0; i<lottoNum; i++) {
     if (lottos[i].indexOf(num) !== -1) {
       winCountCheckArray[i]++;
     }
@@ -93,27 +87,39 @@ function increaseWinNumberCount(num) {
 
 }
 
+function setLuckyNumber(array) {
+  // 6개의 값을 각각 하나씩 찾아야함
+  // 생각이 잘 안남
+
+  // 일단 한개의 데이터(숫자1개~6개까지)를 넘겨줌
+
+  for (let i=0; i<array.length; i++) {
+    increaseWinNumberCount(array[i]);
+  }
+
+  printWinningStatistics(winCountCheckArray);
+}
+
 function printWinningStatistics(array) {
   // 당첨통계 출력
   // switch-case 문을 이용해서
   // 아까 setLuckyNumber 에서 
 
-  var yieldData;
+  let yieldData;
   
-  var matchedNumbers = matchWinNubmer(array);
+  const matchedNumbers = checkWinNubmer(array);
 
   log(winCountCheckArray);
 
   log("당첨 통계");
   log("---------");
-  log("3개 일치 (5000원) - " + matchedNumbers.three);
-  log("4개 일치 (50000원) - " + matchedNumbers.four);
-  log("5개 일치 (1500000원) - " + matchedNumbers.five);
-  log("6개 일치 (2000000000원) - " + matchedNumbers.six);
+  log("3개 일치 (5000원) - " + matchedNumbers[three]);
+  log("4개 일치 (50000원) - " + matchedNumbers[four]);
+  log("5개 일치 (1500000원) - " + matchedNumbers[five]);
+  log("6개 일치 (2000000000원) - " + matchedNumbers[six]);
 
   yieldData = calRateOfReturnToInvestment(matchedNumbers);
-  
-  log(yieldData);
+
   
   // 수익률을 설계할 때 빠트림
   log("나의 수익률은 " + yieldData + "% 입니다.");
@@ -122,36 +128,35 @@ function printWinningStatistics(array) {
 
 function checkWinNubmer(array) {
 
-  var matchedNumber = { three: 0, four: 0, five: 0, six: 0};
+  const matchedNumber = [0, 0, 0, 0];
 
-  for (var i=0; i<lottos.length; i++) {
-    switch(array[i]) {
-      case 3:
-        matchedNumber.three++;
-        break;
-      case 4:
-        matchedNumber.four++;
-        break;
-      case 5:
-        matchedNumber.five++;
-        break;
-      case 6:
-        matchedNumber.six++;
-        break;
-      default:
-        break;
+  for (let i=0; i<lottos.length; i++) {
+    let index = tempFunction(array[i]);
+    if (index === undefined) {
+      continue;
     }
+    matchedNumber[index]++;
   }
 
   return matchedNumber;
 }
 
+function tempFunction(data) {
+  return {
+    3 : 0,
+    4 : 1,
+    5 : 2,
+    6 : 3
+  }[data];
+}
+
 function calRateOfReturnToInvestment(matchedNumbers) {
-  var money = lottos.length * 1000;
-  var totalPrizeMoney = (matchedNumbers.three * prizeMoney.three) +
-                        (matchedNumbers.four * prizeMoney.four) +
-                        (matchedNumbers.five * prizeMoney.five) +
-                        (matchedNumbers.six * prizeMoney.six);
+
+  const money = lottos.length * 1000;
+  const totalPrizeMoney = (matchedNumbers[three] * prizeMoney.three) +
+                        (matchedNumbers[four] * prizeMoney.four) +
+                        (matchedNumbers[five] * prizeMoney.five) +
+                        (matchedNumbers[six] * prizeMoney.six);
 
   return (((totalPrizeMoney - money) / money) * 100).toFixed(0);
 }
