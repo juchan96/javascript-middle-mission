49351256d08e29c0f19e22da536f658bb0e60027
feanar <feanar729@gmail.com>
2018-04-18 15:14:30 +0900

    modify: drinkItems 객체 일부 주석된 데이터 주석 해제 수정

--- vending_machine/drinkItem.js
+++ vending_machine/drinkItem.js
@@ -4,26 +4,26 @@ var drinkItem = [
     "price": 1000,
     "stock": 5
   },
-  // {
-  //   "item": "사이다",
-  //   "price": 1000,
-  //   "stock": 2
-  // },
-  // {
-  //   "item": "포도주스",
-  //   "price": 700,
-  //   "stock": 5
-  // },
-  // {
-  //   "item": "딸기우유",
-  //   "price": 500,
-  //   "stock": 5
-  // },
-  // {
-  //   "item": "미에로화이바",
-  //   "price": 900,
-  //   "stock": 4
-  // },
+  {
+    "item": "사이다",
+    "price": 1000,
+    "stock": 2
+  },
+  {
+    "item": "포도주스",
+    "price": 700,
+    "stock": 5
+  },
+  {
+    "item": "딸기우유",
+    "price": 500,
+    "stock": 5
+  },
+  {
+    "item": "미에로화이바",
+    "price": 900,
+    "stock": 4
+  },
   {
     "item": "물",
     "price": 500,
