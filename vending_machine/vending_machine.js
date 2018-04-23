1b33aaaceb469c37a270c6e5ab3570592357d850
feanar <feanar729@gmail.com>
2018-04-23 18:09:53 +0900

    refactoring & feedback: checkStock()함수 refactoring 진행 및 기타 feedback 사항 보완 수정

--- vending_machine/vending_machine.js
+++ vending_machine/vending_machine.js
@@ -35,16 +35,16 @@ const noticeWord = {
 
 // 동전확인 
 function insertCoin(coin) {
-  const checkType = Object.prototype.toString.call(coin);
+  let checkType = Object.prototype.toString.call(coin);
   let INPUT_NUMBER_ERROR_MSG = "[object Number]";
   if (checkType !== INPUT_NUMBER_ERROR_MSG) return noticeWord.notNumber;
-  
+
   if (coin < 500) {
     console.log(noticeWord.dontBuyDrink + "\n" + coin + noticeWord.returnCoin);
     return coin;
   }
   let actingMachine = searchItem(coin);
-  
+
   console.log(noticeWord.usefulDrink + actingMachine.tableArr);
   return actingMachine;
 }
@@ -52,39 +52,42 @@ function insertCoin(coin) {
 
 // 구매가능 물품 탐색
 function searchItem(coin) {
-  let sellingItem = {
-    productArr : [], // 자판기 내부 활용 배열 데이터 (아이템명 / 가격 / 제고 / 나머지값) 
-    tableArr : [], // 출력용 배열 데이터 ( 아이템명 / 가격 )
+  let sellingItems = {
+    productArr: [], // 자판기 내부 활용 배열 데이터 (아이템명 / 가격 / 제고 / 나머지값) 
+    tableArr: [], // 출력용 배열 데이터 ( 아이템명 / 가격 )
     insertCoin: []
   };
 
   // for & in, for & of, filter, forEach, map메서드의 차이점과 실험 해보기
   for (let sell of itemList) {
     if (coin >= sell.price) {
-      sellingItem.productArr.push(sell);
-      sellingItem.tableArr.push(sell.item + "(" + sell.price + ")");
+      sellingItems.productArr.push(sell);
+      sellingItems.tableArr.push(sell.item + "(" + sell.price + ")");
     }
   }
 
-  sellingItem.insertCoin.push(coin);
-  return sellingItem;
+  sellingItems.insertCoin.push(coin);
+  return sellingItems;
 }
 
 
 // 선택된 아이템이 재고가 있는 지 없는지 확인 하는 함수 
-function checkStock(wantItem, sellingItem) {
+function checkStock(productName, sellingItems) {
   let buyItem = [];
 
-  sellingItem.productArr.filter(product => {
-    if (wantItem === product.item && product.stock === "재고없음") {
-      console.log(product.item + noticeWord.noneStock);
-    } else if (wantItem === product.item && product.stock !== "재고없음") {
-      let restCoin = sellingItem.insertCoin - product.price;
-      buyItem.push(product.item, restCoin);
-      console.log(product.item + noticeWord.outputDrink + "현재잔돈 : " + restCoin);
+  sellingItems.productArr.filter(product => {
+      return productName === product.item;
+    })
+    .forEach(filterProduct => {
+      if (filterProduct.stock === "재고없음") {
+        console.log(filterProduct.item + noticeWord.noneStock);
+      } else {
+        let restCoin = sellingItems.insertCoin - filterProduct.price;
+        buyItem.push(filterProduct.item, restCoin);
+        console.log(filterProduct.item + noticeWord.outputDrink + "현재잔돈 : " + restCoin);
+      }
     }
-  });
-
+  );
   return buyItem;
 }
 
@@ -93,7 +96,7 @@ function checkStock(wantItem, sellingItem) {
 function returnCoin(buyItem, wantReturnCoin) {
   let restCoin = buyItem[1];
 
-  if (restCoin === 0 || restCoin < 500 || wantReturnCoin === "반환") {
+  if (restCoin < 500 || wantReturnCoin === "반환") {
     console.log(restCoin + noticeWord.returnCoin);
   } else {
     insertCoin(restCoin);
@@ -103,8 +106,8 @@ function returnCoin(buyItem, wantReturnCoin) {
 insertCoin(400); // 제품의 구매 금액이 부족합니다. / ...원이 반환 되었습니다
 const sellingItem = insertCoin(1500); // 사용가능한 음료수 목록 => ..... 
 
-checkStock("파워에이드", sellingItem);  // ... 제품은 제고가 업습니다.
+checkStock("파워에이드", sellingItem); // ... 제품은 제고가 업습니다.
 let isRestCoin = checkStock("콜라", sellingItem); // 콜라 상품이 나왔습니다. 현재잔돈 : 500
 
 returnCoin(isRestCoin); // 나머지돈이 구매 불가능: 반환메세지  \ 나머지돈이 구매가능: 사용 가능한 음료수 목록 => 딸기우유(500),물(500)
-returnCoin(isRestCoin, "반환"); // ... 원이 반환 되었습니다. 
+returnCoin(isRestCoin, "반환"); // ... 원이 반환 되었습니다.
