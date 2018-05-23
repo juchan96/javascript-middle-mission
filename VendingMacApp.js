b5cdb7c62114e1147f331c6c27fe8462f97a49be
juchan96 <ghkdwncks96@gmail.com>
2018-05-23 18:14:48 +0900

    Split function in two funtions

--- VendingMacApp.js
+++ VendingMacApp.js
@@ -39,33 +39,59 @@ const beverageData = [
 //금액을 인자로 받고, 사용 가능한 음료의 목록(음료 명, 가격, 재고 수량)을 반환해주는 함수
 function insertCoin(coin, vendingMacData) {
   balance = coin;
-  const availableDrinks = vendingMacData.map((currentVal, index) => {
+  let availableDrinks = [];
+
+  const array = vendingMacData.map((currentVal, index) => {
     if (coin >= currentVal["drinkPrice"]) {
       return currentVal
     }
-
-    return [];
+    return "";
   })
+
+  array.forEach((currentVal) => {
+    if (currentVal !== "") {
+      availableDrinks.push(currentVal);
+    }
+  })
+  
   return availableDrinks;
 }
 
-//음료를 인자로 받아, 나온 음료에대한 메시지를 노출하고, 현재 잔액과, 잔액에 대한 음료 목록을 반환한다.
+//음료를 인자로 받아, 나온 음료에대한 메시지를 노출한다.
 function selectItem(availableDrinkList, drink) {
   let message = "";
   for (let i = 0; i < availableDrinkList.length; i++) {
     if (availableDrinkList[i]["drinkName"] === drink && availableDrinkList[i]["drinkStock"] === 0) {
       message = "재고 없음";
       return message;
-    } else if (availableDrinkList[i]["drinkName"] === drink) {
-    balance = balance - availableDrinkData[i]["drinkPrice"];
-    message = "선택하신 음료, " + drink + "가(이) 나왔습니다.\n" + "현재 잔액: " + balance +"원";
-    return message;
+    }
+
+    else if (availableDrinkList[i]["drinkName"] === drink) {
+      message = "\n선택하신 음료, " + drink + "가(이) 나왔습니다.";
+      return message;
     }
   }
 }
+
+//현재 잔액과, 잔액에 대한 음료 목록을 반환한다.
+function showAvailableDrinkOfBalance(availableDrinkList, drink) {
+  availableDrinkList.forEach((currentVal) => {
+    if (currentVal["drinkName"] === drink) {
+      balance = balance - currentVal["drinkPrice"];
+      const availableDrinkDataOfBalance = insertCoin(balance, beverageData);
+      console.log("현재 잔액: " + balance + "원\n");
+    }
+  })
+
+  const availableDrinkDataOfBalance = insertCoin(balance, beverageData);
+  console.log("사용 가능한 음료 목록: ");
+  return availableDrinkDataOfBalance;
+}
+
 
 let balance = 0;
 const availableDrinkData = insertCoin(1000, beverageData);
 
 console.log(availableDrinkData);
 console.log(selectItem(availableDrinkData, "물"));
+console.log(showAvailableDrinkOfBalance(availableDrinkData,"물"));
