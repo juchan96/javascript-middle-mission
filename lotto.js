1ec8aabc1d252ce9ce7cfdeaa436f91e3279c4a4
juchan96 <ghkdwncks96@gmail.com>
2018-06-12 15:08:45 +0900

    refactoring: split function, fix hardcoding

--- lotto.js
+++ lotto.js
@@ -19,7 +19,8 @@ function buyLottos(coin) {
 // 로또 번호를 무작위로 설정해주는 함수.
 function setLottoNum() {
   const lottoNum = [""];
-  while (lottoNum.length !== 7) {
+  const lottoLength = 7;
+  while (lottoNum.length !== lottoLength) {
     const randomNum = Math.floor(Math.random() * 10 + 1);
     const setLottoNum = lottoNum.includes(randomNum);
     if (!setLottoNum) lottoNum.push(randomNum);
@@ -33,8 +34,9 @@ function setLottoNum() {
 function getWinningNumSum(lottoA, winningNum) {
   let winningNumSum = [];
   let arr = [];
+  const lottoLength = 6;
   for (let lottoNum in lottoA) {
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < lottoLength; i++) {
       if (winningNum.includes(lottoA[lottoNum][i])) {
         arr.push(lottoA[lottoNum][i]);
       }
@@ -49,29 +51,35 @@ function getWinningNumSum(lottoA, winningNum) {
 
 //setLuckyNumber의 반환값을 인자로 받아, 당첨통계를 반환하는 함수.
 function showWinStats(winningNumSum, coin) {
+  const count = showMatchNum(winningNumSum);
   let profit = 0;
   let returnRate = 0;
-  let count3 = 0;
-  let count4 = 0;
-  let count5 = 0;
-  let count6 = 0;
-  winningNumSum.forEach((current) => {
-    if (current === 3) count3++;
-    if (current === 4) count4++;
-    if (current === 5) count5++;
-    if (current === 6) count6++;
-  })
-  profit = count3 * 5000 + count4 * 50000 + count5 * 150000 + count6 * 2000000000;
-  returnRate = (profit / coin) * 100;
-  let message =
-    "3개 일치 (5,000원)- " + count3 + "개" +
-    "\n4개 일치 (50,000원)- " + count4 + "개" +
-    "\n5개 일치 (150,000원)- " + count5 + "개" +
-    "\n6개 일치 (2,000,000,000원)- " + count6 + "개" +
-    "\n나의 총 수익은 " + profit + "원 입니다." +
-    "\n나의 수익률은 " + returnRate + "%입니다."
+
+  profit = count[3] * 5000 + count[4] * 50000 + count[5] * 150000 + count[6] * 2000000000;
+  returnRate = (profit - coin) / coin * 100;
+  let message = `
+    3개 일치 (5,000원)- ${count[3]}개
+    4개 일치 (50,000원)- ${count[4]}개
+    5개 일치 (150,000원)- ${count[5]}개
+    6개 일치 (2,000,000,000원)- ${count[6]}개
+    나의 총 수익은 ${profit}원 입니다.
+    나의 수익률은 ${returnRate}%입니다.`
+
   return message;
 }
 
-// console.log(buyLottos(10000));
+//몇 개 일치하는지 계산하여, 일치하는 개수를 객체로 반환해주는 함수.
+function showMatchNum(winningNumSum) {
+  const count = { "3": 0, "4": 0, "5": 0, "6": 0 };
+  for (let matchNum in count) {
+    winningNumSum.forEach((current) => {
+      if (String(current) === matchNum) {
+        count[matchNum]++;
+      }
+    })
+  }
+
+  return count;
+}
+
 console.log(setLuckyNumber([1, 2, 3, 4, 5, 6], 10000));
